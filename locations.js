0122f8cf70278f7ed9236c85ddc7092791637886
Nicholas Burd <hocliansnb@gmail.com>
2014-11-24 23:23:20 -0500

    Refactoring update
    refactored everyting

--- locations.js
+++ locations.js
@@ -1,233 +1,275 @@
-	var local=0;//keeps track of location
+	var currentLocal;
 	var score=0;//keeps track of total score
-	//checks if player has been to each location
-	var check0=false;
-	var check1=false;
-	var check2=false;
-	var check3=false;
-	var check4=false;
-	var check5=false;
-	var check6=false;
-	var check7=false;
-	var check8=false;
-	var check9=false;
-	var check10=false;
 	var currentItem=0;
 	var inventory=[];
+	var locations=[];
 	//button movement for northern movement
+	
 	function init(){
-		updateDisplay("You wake up to find yourself in a dimly lit room with only a wrench and a moldy old book in your bag. You hear something in the next room.");
+			var item0=new Item();
+			var item1=new Item();
+			var item2=new Item();
+			var item3=new Item();
+			item0.id="0";
+			item0.description="food found on the floor";
+			item0.name="food";
+			
+			item1.id="1";
+			item1.description="staff found on the floor";
+			item1.name="staff";
+			
+			item2.id="2";
+			item2.description="bolt gun found on the floor";
+			item2.name="bolt gun";
+			
+			item3.id="3";
+			item3.description="scraps found on the floor";
+			item3.name="scraps";
+			
+			inventory[0]=item0;
+			inventory[1]=item1;
+			inventory[2]=item2;
+			inventory[3]=item3;
+			
+			var room0=new Location();
+			var room1=new Location();
+			var room2=new Location();
+			var room3=new Location();
+			var room4=new Location();
+			var room5=new Location();
+			var room6=new Location();
+			var room7=new Location();
+			var room8=new Location();
+			var room9=new Location();
+			room0.name="PlaceOfOrigin";
+			room0.message="You wake up to find yourself in a dimly lit room with only a wrench and a moldy old book in your bag. You hear something in the next room.";
+			room0.canGoNorth="false";
+			room0.canGoSouth="true";
+			room0.canGoEast="true";
+			room0.canGoWest="true";
+			room0.id="0";
+			room0.item="0";
+			room0.check=false;
+			
+			room1.name="VoiceRoom";
+			room1.message="The whispers are becoming clearer now. There appears to be two voices. One from the East speaking of magics unknown, and one from the West whispering of blasphemous science.";
+			room1.canGoNorth="false";
+			room1.canGoSouth="true";
+			room1.canGoEast="true";
+			room1.canGoWest="true";
+			room1.id="1";
+			room1.item="0";
+			room1.check=false;
+		
+			room2.name="MachineRoom";
+			room2.message="You arrive in the western room where you find the source of the noise. A mysterious machine sits in the center of the room. its gears turning slowly. what could it do?";
+			room2.canGoNorth="false";
+			room2.canGoSouth="true";
+			room2.canGoEast="false";
+			room2.canGoWest="true";
+			room2.id="2";
+			room2.item="0";
+			room2.check=false;
+		
+			room3.name="Library";
+			room3.message="You have arrived in what appears to be a library of old tomes. One tome catches your eye. It appears so mesmerizing one could just touch it...";
+			room3.canGoNorth="true";
+			room3.canGoSouth="false";
+			room3.canGoEast="false";
+			room3.canGoWest="false";
+			room3.id="3";
+			room3.item="0";
+			room3.check=false;
+		
+			room4.name="BrokenMachineRoom";
+			room4.message="You find yourself faced with a broken machine of unknown purpose. There is a clearly broken piece jutting out of the machine, and a locked door to the east. Being the handsome smart person you are you realize that to proceed you must fix the machine with your tools.";
+			room4.canGoNorth="true";
+			room4.canGoSouth="false";
+			room4.canGoEast="false";
+			room4.canGoWest="false";
+			room4.id="4";
+			room4.item="0";
+			room4.check=false;
+		
+			room5.name="VaultRoom";
+			room5.message="You find yourself in a dusty old room containing nothing but a massive locked vault to the North. There doesnt appear to be any way to unlock the door. Your magic Tome may be able to open it.";
+			room5.canGoNorth="true";
+			room5.canGoSouth="false";
+			room5.canGoEast="false";
+			room5.canGoWest="false";
+			room5.id="5";
+			room5.item="0";
+			room5.check=false;
+		
+			room6.name="DeadRoomMachine";
+			room6.message="After fixing the machine, you entered a new room containing countless skeletons and a highpowered bolt gun. It could be a good idea to take this tool.";
+			room6.canGoNorth="true";
+			room6.canGoSouth="false";
+			room6.canGoEast="false";
+			room6.canGoWest="false";
+			room6.id="6";
+			room6.item="bolt gun";
+			room6.check=false;
+		
+			room7.name="DeadRoom";
+			room7.message="After magically unlocking the door, you enter a new room containing hundreds of skeletons. Among the dead you find a staff which could prove usefull if taken with you.";
+			room7.canGoNorth="true";
+			room7.canGoSouth="false";
+			room7.canGoEast="false";
+			room7.canGoWest="false";
+			room7.id="7";
+			room7.item="staff";
+			room7.check=false;
+		
+			room8.name="pantry";
+			room8.message="You arrive in a pantry filled with food. It could be best to take some for later";
+			room8.canGoNorth="true";
+			room8.canGoSouth="false";
+			room8.canGoEast="false";
+			room8.canGoWest="false";
+			room8.id="8";
+			room8.item="food";
+			room8.check=false;
+		
+			room9.name="pantryEmpty";
+			room9.message="You arrive in a pantry but all the food is missing. All except for a tiny scrap left on the floor. Might as well take it anyway right?";
+			room9.canGoNorth="true";
+			room9.canGoSouth="false";
+			room9.canGoEast="false";
+			room9.canGoWest="false";
+			room9.id="9";
+			room9.item="scrap";
+			room9.check=false;
+		locations[0]=room0;
+		locations[1]=room1;
+		locations[2]=room2;
+		locations[3]=room3;
+		locations[4]=room4;
+		locations[5]=room5;
+		locations[6]=room6;
+		locations[7]=room7;
+		locations[8]=room8;
+		locations[9]=room9;
+		currentL=locations[0].id;
+		updateDisplay(room0.message);
+	}
+	function Location(){
+		this.name="none";
+		this.message="none";
+		this.canGoNorth="none";
+		this.canGoSouth="none";
+		this.canGoEast="none";
+		this.canGoWest="none";
+		this.id="none";
+		this.check=false;
+		this.item="0";
+	}
+	function adjustRoom(){
+		if(!currentL.check){
+			currentL.check=true;
+			score+=5;
+		}
+		document.getElementById("North").disabled=currentL.canGoNorth;
+		document.getElementById("West").disabled=currentL.canGoWest;
+		document.getElementById("South").disabled=currentL.canGoSouth;
+		document.getElementById("East").disabled=currentL.canGoEast;
+		updateDisplay(locations[currentL].message);
 	}
 	function goNorth(){
-		var message;
-		switch(local){
-			case 0: local=1;
-					document.getElementById("North").disabled=true;
-					document.getElementById("West").disabled=false;
-					document.getElementById("South").disabled=false;
-					document.getElementById("East").disabled=false;
-					//checks if player has been to this room before
-					if(!check1){
-						check1=true;
-						score+=5;
-					}
-					//sets and displays the message for this room
-					message=room1(0);
-					updateDisplay(message);
+		switch(currentL=parseInt(currentL)){
+			case 0: currentL=locations[1].id;
 				break;
-			case 2: local=4;
-					document.getElementById("East").disabled=true;
-					document.getElementById("West").disabled=true;
-					document.getElementById("North").disabled=false;
-					document.getElementById("South").disabled=false;
-					if(!check4){
-						check4=true;
-						score+=5;
-					}
-					//sets and displays the message for this room
-					message=room4(2);
-					updateDisplay(message);
+			case 2: currentL=locations[4].id;
 				break;
-			case 3: local=5;
-					document.getElementById("East").disabled=true;
-					document.getElementById("West").disabled=true;
-					document.getElementById("North").disabled=false;
-					document.getElementById("South").disabled=false;
-					if(!check5){
-						check5=true;
-						score+=5;
-					}
-					//sets and displays the message for this room
-					message=room5(3);
-					updateDisplay(message);
+			case 3: currentL=locations[5].id;
 				break;
-			case 4: local=6;
-					document.getElementById("North").disabled=true;
-					document.getElementById("West").disabled=true;
-					document.getElementById("East").disabled=false;
-					document.getElementById("South").disabled=false;
-					if(!check6){
-						check6=true;
-						score+=5;
-					}
-					//sets and displays the message for this room
-					message=room6(4);
-					updateDisplay(message);
+			case 4: currentL=locations[6].id;
 					currentItem=1;
 				break;
-			case 5: local=7;
-					document.getElementById("North").disabled=true;
-					document.getElementById("East").disabled=true;
-					document.getElementById("West").disabled=false;
-					document.getElementById("South").disabled=false;
-					if(!check7){
-						chck7=true;
-						score+=5;
-					}
-					//sets and displays the message for this room
-					message=room7(5);
-					updateDisplay(message);
+			case 5: currentL=locations[7].id;
 					currentItem=2;
 				break;
 			default: 
 					//sets and displays error message for wrong movement
-					message="You can't go that way.";
-					updateDisplay(message);
+					updateDisplay("You can't go that way");
 				break;
 		}
+		adjustRoom();
 		return;
 	}
 	//button movement for southern movement
 	function goSouth(){
-		var message;
-		//checks if movement to room 0 is possible
-		switch(local){
-			case 1: local=0;
-					document.getElementById("East").disabled=true;
-					document.getElementById("South").disabled=true;
-					document.getElementById("West").disabled=true;
-					document.getElementById("North").disabled=false;
-					//checks if player has been to this room before
-					if(check0==false){
-						check0=true;
-						score+=5;
-					}
-					//sets and displays the message for this room
-					message=room0(1);
-					updateDisplay(message);
+		switch(currentL=parseInt(currentL)){
+			case 1: currentL=locations[0].id;
+				break;
+			case 5: currentL=locations[3].id;
+				break;
+			case 4: currentL=locations[2].id;
+				break;
+			case 6: currentL=locations[4].id;
+				break;
+			case 7: currentL=locations[5].id;
 				break;
 			default: 
 					//sets and displays error message for wrong movement
-					message="You can't go that way.";
-					updateDisplay(message);
+					updateDisplay("you can't go that way.");
 				break;
 		}
+		adjustRoom();
 		return;
 	}
 	//button function for eastern movement
 	function goEast(){
-		var message;
-		switch(local){
+		switch(currentL){
 			case 1: //checks if movement to room 3 is possible
-					local=3;
-					document.getElementById("East").disabled=true;
-					document.getElementById("South").disabled=true;
-					document.getElementById("West").disabled=false;
-					document.getElementById("North").disabled=false;
-					//checks if player has been to this room before
-					if(!check3){
-						check3=true;
-						score+=5;
-					}
-					//sets and displays the message for this room
-					message=room3(1);
-					updateDisplay(message);
+					currentL=locations[3].id;
 				break;
 			case 2: //checks if movement to room 1 is possible
-					local=1;
-					document.getElementById("North").disabled=true;
-					document.getElementById("West").disabled=false;
-					document.getElementById("South").disabled=false;
-					document.getElementById("East").disabled=false;
-					//sets and displays the message for this room
-					message=room1(2);
-					updateDisplay(message);
+					currentL=locations[1].id;
 				break;
 			case 6: //checks if movement to room 8 is possible
-					local=8;
-					document.getElementById("North").disabled=true;
-					document.getElementById("South").disabled=true;
-					document.getElementById("East").disabled=true;
-					document.getElementById("West").disabled=false;
-					//checks if player has been to this room before
-					if(!check8){
-						check8=true;
-						score+=5;
-					}
-					//sets and displays the message for this room
-					message=room8(6);
-					updateDisplay(message);
+					currentL=locations[8].id;
 					currentItem=3;
 				break;
+			case 9: //checks if movement to room 7 is possible
+					currentL=locations[7].id;
+				break;
 			default: //sets and displays error message for wrong movement
-					message="You can't go that way.";
-					updateDisplay(message);
+					updateDisplay("You can't go that way.");
 				break;
 		}
+		adjustRoom();
 		return;
 	}
 	//button function for western movement
 	function goWest(){
 		var message;
-		switch(local){
+		switch(currentL){
 			case 1: //checks if movement to room 2 is possible
-					local=2;
-					document.getElementById("West").disabled=true;
-					document.getElementById("South").disabled=true;
-					document.getElementById("North").disabled=false;
-					document.getElementById("East").disabled=false;
-					//checks if player has been to this room before
-					if(!check2){
-						check2=true;
-						score+=5;
-					}
-					//sets and displays the message for this room
-					message=room2(1);
-					updateDisplay(message);
+					currentL=locations[2].id;
 				break;
 			case 3: //checks if movement to room 1 is possible
-					local=1;
-					document.getElementById("North").disabled=true;
-					document.getElementById("West").disabled=false;
-					document.getElementById("South").disabled=false;
-					document.getElementById("East").disabled=false;
-					message=room1(3);
-					updateDisplay(message);
+					currentL=locations[1].id;
 				break;
 			case 7: //checks if movement to room 9 is possible
-					local=9;
-					document.getElementById("North").disabled=true;
-					document.getElementById("South").disabled=true;
-					document.getElementById("West").disabled=true;
-					document.getElementById("East").disabled=false;
-					//checks if player has been to this room before
-					if(!check9){
-						check9=true;
-						score+=5;
-					}
-					//sets and displays the message for this room
-					message=room9(7);
-					updateDisplay(message);
+					currentL=locations[9].id;
 					currentItem=4;
 				break;
+			case 8: currentL=locations[6].id;
+				break;
 			default: //sets and displays error message for wrong movement
-					message="You can't go that way.";
-					updateDisplay(message);
+					updateDisplay("You can't go that way.");
 				break;
 		}
+		adjustRoom();
 		return;
 	}
 	//uses text input to trigger movement functions
+	function Item(){
+		this.id=""
+		this.description="";
+		this.name="";
+	}
 	function goButton(){
 		var input=document.getElementById("inputTxtBox").value;
 		//checks for North movement
@@ -265,69 +307,7 @@
 			updateDisplay("Invalid Command. Valid Commands are: n,s,e,w,N,S,E,W");
 		}
 	}
-	function room0(cRoom){
-		switch(cRoom){
-			case 1: return "You seem to be back where you started. Best be careful not to get lost in this labyrinth of a place.";
-				break;
-		}
-	}
-	function room1(cRoom){
-		var msg="You seem to be back in the main hall. Best be careful not to get lost in this labyrinth of a place.";
-		switch(cRoom){
-			case 0: return "The whispers are becoming clearer now. There appears to be two voices. One from the East speaking of magics unknown, and one from the West whispering of blasphemous science.";
-				break;
-			case 3: return msg;
-				break;
-			case 2: return msg;
-				break;
-		}
-	}
-	function room2(cRoom){
-		switch(cRoom){
-			case 2: return "You arrive in the western room where you find the source of the noise. A mysterious machine sits in the center of the room. its gears turning slowly. what could it do?";
-				break;
-		}
-	}
-	function room3(cRoom){
-		switch(cRoom){
-			case 1: return "You have arrived in what appears to be a library of old tomes. One tome catches your eye. It appears so mesmerizing one could just touch it...";
-				break;
-		}
-	}
-	function room4(cRoom){
-		switch(cRoom){
-			case 2: return "You find yourself faced with a broken machine of unknown purpose. There is a clearly broken piece jutting out of the machine, and a locked door to the east. Being the handsome smart person you are you realize that to proceed you must fix the machine with your tools.";
-				break;
-		}
-	}
-	function room5(cRoom){
-		switch(cRoom){
-			case 3: return "You find yourself in a dusty old room containing nothing but a massive locked vault to the North. There doesnt appear to be any way to unlock the door. Your magic Tome may be able to open it.";
-				break;
-		}
-	}
-	function room6(cRoom){
-		switch(cRoom){
-			case 4: return "After fixing the machine, you entered a new room containing countless skeletons and a highpowered bolt gun. It could be a good idea to take this tool.";
-				break;
-		}
-	}
-	function room7(cRoom){
-		switch(cRoom){
-			case 5: return "After magically unlocking the door, you enter a new room containing hundreds of skeletons. Among the dead you find a staff which could prove usefull if taken with you.";
-				break;
-		}
-	}
-	function room8(cRoom){
-		switch(cRoom){
-			case 6: return "You arrive in a pantry filled with food. It could be best to take some for later";
-		}
-	}
-	function room9(cRoom){
-		switch(cRoom){
-			case 7: return "You arrive in a pantry but all the food is missing. All except for a tiny scrap left on the floor. Might as well take it anyway right?";
-		}
-	}
+	
 	//this function displays story messages and the score
 	function updateDisplay(msg){
 		//displays story
