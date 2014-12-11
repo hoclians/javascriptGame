69a7a496383b477837067b19874399969bca4665
Nicholas Burd <hocliansnb@gmail.com>
2014-12-11 11:42:24 -0500

    final update
    added puzzle element
    finished item code
    finished project

--- locations.js
+++ locations.js
@@ -1,35 +1,51 @@
-	var currentLocal;
+	var currentL=0;
 	var score=0;//keeps track of total score
-	var currentItem=0;
 	var inventory=[];
 	var locations=[];
-	//button movement for northern movement
+    var NORTH = 0;
+    var SOUTH = 1;
+    var EAST  = 2;
+    var WEST  = 3;
+    var nav = [ // N   S   E   W 
+      /* 0 */ [1,-1,-1,-1],
+      /* 1 */ [-1,0,3,2],
+      /* 2 */ [4,-1,-1,1],
+      /* 3 */ [5,-1,-1,1], 
+      /* 4 */ [6,2,-1,-1],
+	  /* 5 */ [7,3,-1,-1],
+	  /* 6 */ [-1,4,8,-1],
+      /* 7 */ [-1,5,-1,9],
+      /* 8 */ [-1,-1,10,6],
+      /* 9 */ [-1,-1,7,10],
+	  /* 10*/ [-1,-1,9,8]
+	  
+	  ];
 	
 	function init(){
 			var item0=new Item();
 			var item1=new Item();
 			var item2=new Item();
 			var item3=new Item();
-			item0.id="0";
+			item0.id=1;
 			item0.description="food found on the floor";
 			item0.name="food";
 			
-			item1.id="1";
+			item1.id=2;
 			item1.description="staff found on the floor";
 			item1.name="staff";
 			
-			item2.id="2";
+			item2.id=3;
 			item2.description="bolt gun found on the floor";
 			item2.name="bolt gun";
 			
-			item3.id="3";
+			item3.id=4;
 			item3.description="scraps found on the floor";
 			item3.name="scraps";
 			
-			inventory[0]=item0;
-			inventory[1]=item1;
-			inventory[2]=item2;
-			inventory[3]=item3;
+			//inventory[0]=item0.id;
+			//inventory[1]=item1.id;
+			//inventory[2]=item2.id;
+			//inventory[3]=item3.id;
 			
 			var room0=new Location();
 			var room1=new Location();
@@ -41,6 +57,8 @@
 			var room7=new Location();
 			var room8=new Location();
 			var room9=new Location();
+			var room10=new Location();
+			
 			room0.name="PlaceOfOrigin";
 			room0.message="You wake up to find yourself in a dimly lit room with only a wrench and a moldy old book in your bag. You hear something in the next room.";
 			room0.canGoNorth="false";
@@ -109,6 +127,7 @@
 			room6.canGoWest="false";
 			room6.id="6";
 			room6.item="bolt gun";
+			room6.currentItem=3;
 			room6.check=false;
 		
 			room7.name="DeadRoom";
@@ -119,6 +138,7 @@
 			room7.canGoWest="false";
 			room7.id="7";
 			room7.item="staff";
+			room7.currentItem=2;
 			room7.check=false;
 		
 			room8.name="pantry";
@@ -129,6 +149,7 @@
 			room8.canGoWest="false";
 			room8.id="8";
 			room8.item="food";
+			room8.currentItem=1;
 			room8.check=false;
 		
 			room9.name="pantryEmpty";
@@ -139,20 +160,51 @@
 			room9.canGoWest="false";
 			room9.id="9";
 			room9.item="scrap";
+			room9.currentItem=4;
 			room9.check=false;
-		locations[0]=room0;
-		locations[1]=room1;
-		locations[2]=room2;
-		locations[3]=room3;
-		locations[4]=room4;
-		locations[5]=room5;
-		locations[6]=room6;
-		locations[7]=room7;
-		locations[8]=room8;
-		locations[9]=room9;
-		currentL=locations[0].id;
-		updateDisplay(room0.message);
+			
+			room10.name="finalBoss";
+			room10.message="A massive beast stands before you guarding the exit what should you do?";
+			room10.canGoNorth="true";
+			room10.canGoSouth="true";
+			room10.canGoEast="false";
+			room10.canGoWest="false";
+			room10.id="10";
+			room10.item="none";
+			room10.check=false;
+			
+			locations[0]=room0;
+			locations[1]=room1;
+			locations[2]=room2;
+			locations[3]=room3;
+			locations[4]=room4;
+			locations[5]=room5;
+			locations[6]=room6;
+			locations[7]=room7;
+			locations[8]=room8;
+			locations[9]=room9;
+			locations[10]=room10;
+			
+			updateDisplay(room0.message);
 	}
+	//sets the location you are moving to
+	function nextLoc(dir){
+		var d=1;
+		switch(dir){
+			case 0: d=0;break;
+			case 1: d=1;break;
+			case 2: d=2;break;
+			case 3: d=3;break;
+			default: "no invalid";
+		}
+		var newLocal=nav[currentL][d];
+		if(newLocal>=0){
+			currentL=newLocal;
+		}else{
+			updateDisplay("cannot go that way");
+		}
+	}
+	//all rooms are created from this Class
 	function Location(){
 		this.name="none";
 		this.message="none";
@@ -162,8 +214,10 @@
 		this.canGoWest="none";
 		this.id="none";
 		this.check=false;
+		this.currentItem;
 		this.item="0";
 	}
+	//set message for current room
 	function adjustRoom(){
 		if(!currentL.check){
 			currentL.check=true;
@@ -175,98 +229,33 @@
 		document.getElementById("East").disabled=currentL.canGoEast;
 		updateDisplay(locations[currentL].message);
 	}
+	//northern button movement
 	function goNorth(){
-		switch(currentL=parseInt(currentL)){
-			case 0: currentL=locations[1].id;
-				break;
-			case 2: currentL=locations[4].id;
-				break;
-			case 3: currentL=locations[5].id;
-				break;
-			case 4: currentL=locations[6].id;
-					currentItem=1;
-				break;
-			case 5: currentL=locations[7].id;
-					currentItem=2;
-				break;
-			default: 
-					//sets and displays error message for wrong movement
-					updateDisplay("You can't go that way");
-				break;
-		}
+		nextLoc(NORTH);
 		adjustRoom();
 		return;
 	}
-	//button movement for southern movement
+	//southern button movement
 	function goSouth(){
-		switch(currentL=parseInt(currentL)){
-			case 1: currentL=locations[0].id;
-				break;
-			case 5: currentL=locations[3].id;
-				break;
-			case 4: currentL=locations[2].id;
-				break;
-			case 6: currentL=locations[4].id;
-				break;
-			case 7: currentL=locations[5].id;
-				break;
-			default: 
-					//sets and displays error message for wrong movement
-					updateDisplay("you can't go that way.");
-				break;
-		}
+		nextLoc(SOUTH);
 		adjustRoom();
 		return;
 	}
-	//button function for eastern movement
+	//eastern button movement
 	function goEast(){
-		switch(currentL){
-			case 1: //checks if movement to room 3 is possible
-					currentL=locations[3].id;
-				break;
-			case 2: //checks if movement to room 1 is possible
-					currentL=locations[1].id;
-				break;
-			case 6: //checks if movement to room 8 is possible
-					currentL=locations[8].id;
-					currentItem=3;
-				break;
-			case 9: //checks if movement to room 7 is possible
-					currentL=locations[7].id;
-				break;
-			default: //sets and displays error message for wrong movement
-					updateDisplay("You can't go that way.");
-				break;
-		}
+		nextLoc(EAST);
 		adjustRoom();
 		return;
 	}
-	//button function for western movement
+	//western button movement
 	function goWest(){
-		var message;
-		switch(currentL){
-			case 1: //checks if movement to room 2 is possible
-					currentL=locations[2].id;
-				break;
-			case 3: //checks if movement to room 1 is possible
-					currentL=locations[1].id;
-				break;
-			case 7: //checks if movement to room 9 is possible
-					currentL=locations[9].id;
-					currentItem=4;
-				break;
-			case 8: currentL=locations[6].id;
-				break;
-			default: //sets and displays error message for wrong movement
-					updateDisplay("You can't go that way.");
-				break;
-		}
+		nextLoc(WEST);
 		adjustRoom();
 		return;
 	}
 	//uses text input to trigger movement functions
 	function Item(){
-		this.id=""
+		this.id;
 		this.description="";
 		this.name="";
 	}
@@ -289,22 +278,32 @@
 			goWest();
 		}
 		else if(input=="help"||input=="Help"){
-			updateDisplay("Valid Commands are: n,s,e,w,N,S,E,W,take,Take,help,Help");
+			updateDisplay("Valid Commands are: n,s,e,w,N,S,E,W,take,Take,help,Help,inventory,Inventory,slay,Slay");
+		}
+		else if(input=="inventory"||input=="Inventory"){
+			dispInventory();
+		}
+		else if(input=="slay"||input=="Slay"){
+			if(inventory[1]==2||inventory[2]==3){
+				updateDisplay("THE BEAST HAS BEEN SLAIN DUE TO YOUR KLEPTOMANIA!");
+			} else{
+				updateDisplay("YOU DONT HAVE THE REQUIRED ITEM! THE BEAST THROWS YOU OUT OF THE ROOM.");
+			}
 		}
 		else if(input=="take"||input=="Take"){
-			switch(currentItem){
-				case 1: inventory[0]=currentItem;
+			switch(locations[currentL].currentItem){
+				case 1: inventory[0]=locations[currentL].currentItem;
 					break;
-				case 2: inventory[1]=currentItem;
+				case 2: inventory[1]=locations[currentL].currentItem;
 					break;
-				case 3: inventory[2]=currentItem;
+				case 3: inventory[2]=locations[currentL].currentItem;
 					break;
-				case 4: inventory[3]=currentItem;
+				case 4: inventory[3]=locations[currentL].currentItem;
 					break;
 			}
 		}else{
 			//Error Message for invalid input
-			updateDisplay("Invalid Command. Valid Commands are: n,s,e,w,N,S,E,W");
+			updateDisplay("Invalid Command. Valid Commands are: n,s,e,w,N,S,E,W,take,Take,help,Help,inventory,Inventory,slay,Slay");
 		}
 	}
 	
@@ -322,7 +321,7 @@
 		var i3="";
 		var i4="";
 		if(inventory[0]==1){
-			i1="food"
+			i1="food";
 		}
 		if(inventory[1]==2){
 			i2="staff";
